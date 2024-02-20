da8706af8c1cd6137a189d6db93525994ac0551e
Abir Santra <santraabir8@gmail.com>
2024-02-20 23:14:29 +0530

    feat: topbar design complete

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { INITIAL_USER, useUserContext } from "@/context/AuthContext";
+import Logo from "./logo";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useNavigate } from "react-router-dom";
+import { LogOut, Settings } from "lucide-react";
+import { useSignOutUser } from "@/lib/tanstack-query/queries";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
 
 const TopBar = () => {
-  return <div className="sticky top-0 z-50 w-full md:hidden">TopBar</div>;
+  const { user, setUser, setIsAuthenticated } = useUserContext();
+  const navigate = useNavigate();
+  const avatarFallback = user.fullName.charAt(0).toUpperCase();
+  const { mutate: signOutUser } = useSignOutUser();
+
+  const handleLogout = async () => {
+    try {
+      signOutUser();
+      setIsAuthenticated(false);
+      setUser(INITIAL_USER);
+      navigate("/sign-in");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast("Signed Out Successfully!");
+      }
+      console.log("Error: ", error);
+    }
+  };
+
+  return (
+    <div className="sticky top-0 z-50 flex h-16 w-full items-center justify-between px-4 shadow-sm md:hidden">
+      <Logo />
+      <DropdownMenu>
+        <DropdownMenuTrigger className="ml-auto">
+          <Avatar className="h-8 w-8 border border-gray-300">
+            <AvatarImage src={user.avatar} />
+            <AvatarFallback>{avatarFallback}</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mb-4 w-36 font-poppins" align="end">
+          <DropdownMenuItem className="flex gap-2 p-3 text-xs font-medium text-gray-700">
+            <Settings size={16} /> Edit Profile
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex gap-2 p-3 text-xs font-medium text-gray-700"
+            onClick={handleLogout}
+          >
+            <LogOut size={16} /> Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 };
 
 export default TopBar;
