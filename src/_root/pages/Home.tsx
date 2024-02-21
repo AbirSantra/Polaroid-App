5af9ab78a0195ed36bd137d54d77731c20901b16
Abir Santra <santraabir8@gmail.com>
2024-02-21 23:50:54 +0530

    feat: update add post modal design

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/AuthContext";
+import { useModal } from "@/context/ModalContext";
 import { Flame, Heart } from "lucide-react";
 import { useState } from "react";
 
@@ -7,10 +10,15 @@ const Home = () => {
     "FOLLOWING"
   );
 
+  const { user } = useUserContext();
+  const avatarFallback = user.fullName.charAt(0).toUpperCase();
+
+  const { openModal } = useModal();
+
   return (
     <div className="custom-scrollbar flex flex-1 flex-col gap-4 overflow-scroll p-8">
       {/* Feed Type Toggler */}
-      <div className="flex w-full text-base text-gray-500">
+      <div className="mb-4 flex w-full text-base text-gray-500">
         <Button
           variant={"ghost"}
           className={`flex flex-1 items-center justify-center gap-2 hover:bg-transparent hover:text-rose-500 ${feedType === "FOLLOWING" && "font-semibold text-rose-500"}`}
@@ -30,9 +38,24 @@ const Home = () => {
       </div>
 
       {/* Create Post */}
-      <div>Create Post</div>
+      <div
+        className="hidden w-full items-center justify-center gap-4 rounded-md border p-4 md:flex"
+        onClick={() => openModal("NEW-POST")}
+      >
+        <Avatar className="h-12 w-12 border border-gray-300">
+          <AvatarImage src={user.avatar} />
+          <AvatarFallback>{avatarFallback}</AvatarFallback>
+        </Avatar>
+        <p className="w-full cursor-text text-sm font-medium text-gray-500">
+          What's new?
+        </p>
+        <Button variant={"primary"} className="ml-auto w-fit px-4 py-2">
+          Post
+        </Button>
+      </div>
+
       {/* Feed */}
-      <div className="min-h-[150vh] bg-rose-100">Feed</div>
+      <div className="p min-h-[150vh] rounded-md border">Feed</div>
     </div>
   );
 };
--- src/components/add-post-modal.tsx
+++ src/components/add-post-modal.tsx
@@ -2,14 +2,13 @@ import { useModal } from "@/context/ModalContext";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Label } from "@radix-ui/react-label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { ImagePlus } from "lucide-react";
 
 const AddPostModal = () => {
   const { modalState, closeModal } = useModal();
@@ -21,29 +20,23 @@ const AddPostModal = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="font-poppins sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
+          <DialogTitle className="">New Post</DialogTitle>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+        <div className="flex flex-col gap-4">
+          <Input
+            placeholder="What's new?"
+            className="max-w-none border-none p-0 text-sm font-light"
+          />
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <div className="mr-auto flex cursor-pointer items-center justify-center text-gray-400 hover:text-gray-500">
+            <ImagePlus size={18} />
+          </div>
+          <Button variant={"primary"} className="w-fit px-4 py-2 font-poppins">
+            Post
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
