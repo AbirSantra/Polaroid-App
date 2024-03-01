cdc83e2e74b724bd9a9a12b5a018d1ec42407dfb
Abir Santra <santraabir8@gmail.com>
2024-03-01 18:30:00 +0530

    Reset edit profile modal

--- src/components/edit-profile-modal.tsx
+++ src/components/edit-profile-modal.tsx
@@ -21,31 +21,7 @@ const EditProfileModal = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
+      <DialogContent className="sm:max-w-[425px]">Edit Profile</DialogContent>
     </Dialog>
   );
 };
