10036ab579fad3bb86e340db150ef343eca89bed
Abir Santra <santraabir8@gmail.com>
2024-03-29 15:35:53 +0530

    fix auth layout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -6,32 +6,28 @@ import { useUserContext } from "@/context/AuthContext";
 const AuthLayout = () => {
   const { isAuthenticated } = useUserContext();
 
-  return (
-    <>
-      {isAuthenticated ? (
-        <Navigate to="/" />
-      ) : (
-        <>
-          <section className="hidden h-screen w-1/2 flex-col items-center justify-center bg-gray-50 py-32 font-poppins md:flex">
-            {/* Logo */}
-            <div className="my-auto flex items-center gap-4">
-              <div className="flex h-16 w-16 items-center justify-center">
-                <img src={PolaroidLogo} alt="" />
-              </div>
-              <h1 className="text-4xl font-bold">Polaroid</h1>
-            </div>
-            {/* Tagline */}
-            <div className="text-center font-semibold text-gray-500">
-              <p>Capture Moments.</p>
-              <p>Share Stories.</p>
-            </div>
-          </section>
-          <section className="flex flex-1 flex-col items-center justify-center">
-            <Outlet />
-          </section>
-        </>
-      )}
-    </>
+  return isAuthenticated ? (
+    <Navigate to="/" />
+  ) : (
+    <div className="flex h-screen w-full">
+      <section className="hidden h-screen w-1/2 flex-col items-center justify-center bg-gray-50 py-32 font-poppins md:flex">
+        {/* Logo */}
+        <div className="my-auto flex items-center gap-4">
+          <div className="flex h-16 w-16 items-center justify-center">
+            <img src={PolaroidLogo} alt="" />
+          </div>
+          <h1 className="text-4xl font-bold">Polaroid</h1>
+        </div>
+        {/* Tagline */}
+        <div className="text-center font-semibold text-gray-500">
+          <p>Capture Moments.</p>
+          <p>Share Stories.</p>
+        </div>
+      </section>
+      <section className="flex h-full flex-1 flex-col items-center justify-center">
+        <Outlet />
+      </section>
+    </div>
   );
 };
 
